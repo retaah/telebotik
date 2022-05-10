ce2a4f445993bbef964295aaa2ed44eaf4e261d1
Maksim <Maskim.lion.by@gmail.com>
2022-05-10 18:25:06 +0300

    Adding correct command handler

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 import telebot
 import requests as r
 
-bot = telebot.TeleBot('user_token')
+bot = telebot.TeleBot('5114733697:AAElaHjA30DWQFnfmCON5eMoG4RwJCyyDVU')
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == 'Hello':
-        answer = 'Hello, how can i help you?'
-        bot.send_message(message.from_user.id, answer)
-    elif message.text == '/help':
-        answer = 'Write "Hello"'
-        bot.send_message(message.from_user.id, answer)
-    elif message.text == '/check_github':
-        response = r.get('https://github.com')
-        if response.status_code == 200:
-            bot.send_message(message.from_user.id, 'Accessible!')
-        else:
-            bot.send_message(message.from_user.id, 'Fail!')
-    else:
-        answer = 'I dont understand you!'
-        bot.send_message(message.from_user.id, answer)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    with open('assets/greeting.txt') as file:
+        greeting = file.read()
+    bot.send_message(message.chat.id, greeting)
+
 
 
 
